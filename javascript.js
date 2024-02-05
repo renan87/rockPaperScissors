7524f67699f55e972901ec9096fbec02bb515429
Renan Pacini <renanpacini@gmail.com>
2024-02-05 18:43:59 -0300

    Add function to get Player choice via prompt. Add function to randomly generate computer choice. Add function to emulate a round of RPS. In construction.

--- javascript.js
+++ javascript.js
@@ -10,16 +10,37 @@ function getRandomNumber() {
     return (Math.floor(Math.random() * (max - min + 1)) + min);
 }
 
-function getComputerChoice (randomNumber) {
+function getComputerChoice(randomNumber) {
     switch(randomNumber) {
         case 1:
             return "Rock";
         case 2:
             return "Paper";
         case 3:
-            return "Scissors"
+            return "Scissors";
     }
 }
 
-let computerChoice = getComputerChoice (getRandomNumber());
+function getPlayerChoice() {
+    let string = prompt("Please enter either Rock, Paper or Scissors as your choice:");
+   
+    while (string !== "Rock" && string !== "Paper" && string !== "Scissors")
+        string = prompt("Entered wrong value. Please enter either Rock, Paper or Scissors as your choice:")
+        return string;
+}
+
+function playRPS(humanPlayer,aiPlayer) {
+    while (humanPlayer === aiPlayer) {
+        console.log("It is a tie, play again.");
+        humanPlayer = getPlayerChoice();
+        aiPlayer = getComputerChoice();
+    }
+}
+
+
+let computerChoice = getComputerChoice(getRandomNumber());
+let playerChoice = getPlayerChoice();
+
+
 console.log(computerChoice);
+console.log(playerChoice);
